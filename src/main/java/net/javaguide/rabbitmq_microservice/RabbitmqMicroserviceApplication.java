fa4c108c7191ed357f48d745694924d798f55260
Otavio Benedicto <otavio.benedicto@webjump.com.br>
2023-12-08 11:26:18 -0300

    start project with rabbitmq

---
+++ src/main/java/net/javaguide/rabbitmq_microservice/RabbitmqMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package net.javaguide.rabbitmq_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguide/rabbitmq_microservice/RabbitmqMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguide.rabbitmq_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
