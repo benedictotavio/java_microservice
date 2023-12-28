e62ec6c1be97f01842add4d96af2db0bdd83d293
Otavio Benedicto <otavio.bene@outlook.com>
2023-12-28 12:50:24 -0300

    remove dog_control

--- dog_control/src/main/java/com/br/dog_control/repository/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.br.dog_control.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.dog_control.models.Owner;
-
-public interface OwnerRepository extends JpaRepository<Owner, String> {}
